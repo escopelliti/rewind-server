f3526394860ab7888f5a3798e56388d82d120817
escopelliti <en.scopelliti@gmail.com>
2014-10-05 16:57:51 +0200

    Draft Implementation for Connection Module

---
+++ PDSProject/PDSProject/ClientServerCommunicationManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CommunicationLibrary
+{
+    class ClientServerCommunicationManager
+    {
+        protected static Socket socket;
+        protected AsyncCallback connectHandler;
+        protected AsyncCallback sendHandler;
+        public static AsyncCallback receiveHandler;
+        protected static byte[] bytesRead;
+        public static ManualResetEvent dataReady = new ManualResetEvent(false);
+
+        public void Send(byte[] toSend)
+        {
+            socket.BeginSend(toSend, 0, toSend.Length, 0, sendHandler, socket);
+        }
+
+        public void SendObject(object toSend)
+        {
+            //Serialization and then send
+            //clientSocket.BeginSend(toSend, 0, toSend.Length, 0, sendHandler, clientSocket);
+        }
+
+        protected static void SendCallback(IAsyncResult iar)
+        {
+            Socket client = (Socket)iar.AsyncState;
+            client.EndSend(iar);
+        }
+
+        public void SendFiles(List<string> filepathList)
+        {
+            foreach (string fileName in filepathList)
+            {
+                socket.SendFile(fileName);
+            }
+        }
+
+        public void Receive()
+        {
+            SocketObject socketObj = new SocketObject();
+            socketObj.socket = socket;
+            socketObj.receiveBuffer = new byte[SocketObject.bufferSize];
+            socket.BeginReceive(socketObj.receiveBuffer, 0, SocketObject.bufferSize, 0, receiveHandler, socketObj);
+        }
+
+        private void ReceiveObject()
+        {
+
+            //deserializzazione
+        }
+        public struct SocketObject
+        {
+            public Socket socket;
+            public const int bufferSize = 1024;
+            public byte[] receiveBuffer;
+        }
+    }
+}
---
+++ PDSProject/PDSProject/ServerCommunicationManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CommunicationLibrary
+{
+    class ServerCommunicationManager : ClientServerCommunicationManager
+    {
+        private IPEndPoint localEP;
+        private AsyncCallback acceptingSocketHandler;
+        private static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public ServerCommunicationManager(string host, int port, ProtocolType protocolType)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(host);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            localEP = new IPEndPoint(ipAddress, port);
+            socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, protocolType);
+            acceptingSocketHandler += AcceptSocketCallback;
+        }
+
+        private static void AcceptSocketCallback(IAsyncResult iar)
+        {
+
+            allDone.Set();
+            Socket serverSocket = (Socket)iar.AsyncState;
+            socket = serverSocket.EndAccept(iar);
+            SocketObject socketObj = new SocketObject();
+            socketObj.receiveBuffer = new byte[SocketObject.bufferSize];
+            socketObj.socket = socket;
+            bytesRead = new byte[SocketObject.bufferSize];
+            socket.BeginReceive(socketObj.receiveBuffer, 0, SocketObject.bufferSize, 0, receiveHandler, socketObj);
+        }
+
+        public void Listen()
+        {
+            try
+            {
+                socket.Bind(localEP);
+                socket.Listen(100);
+                while (true)
+                {
+                    allDone.Reset();
+                    socket.BeginAccept(acceptingSocketHandler, socket);
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
